44ec6fbcc920e30939c400795dbf143f069f2c37
Max <Max@MAX-LENOVO>
2022-07-06 14:50:19 +0800

    Added matching java config for route.

--- src/main/java/ph/mramos/booking/gatewayserver/Application.java
+++ src/main/java/ph/mramos/booking/gatewayserver/Application.java
@@ -2,12 +2,27 @@ package ph.mramos.booking.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+//	@Bean
+	public RouteLocator routLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("booking-app-route",
+						r -> r.path("/booking/**")
+							.and().method("GET", "POST", "PUT", "DELETE")
+						.filters(
+							f -> f.addRequestHeader("X-Request-Color", "red")
+							.addResponseHeader("X-Response-Color", "blue"))
+						.uri("http://localhost:9080/"))
+				.build();
 	}
 
 }
