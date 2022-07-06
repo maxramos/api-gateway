31fb6250e36225e70dd9f3709539bec4afeef928
Max <Max@MAX-LENOVO>
2022-07-07 02:10:57 +0800

    Updated route config.

--- src/main/java/ph/mramos/booking/apigateway/Application.java
+++ src/main/java/ph/mramos/booking/apigateway/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class Application {
@@ -12,7 +15,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
+	@Bean
 	public RouteLocator routLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route("booking-app-route",
@@ -22,6 +25,17 @@ public class Application {
 							f -> f.addRequestHeader("X-Request-Color", "red")
 							.addResponseHeader("X-Response-Color", "blue"))
 						.uri("http://localhost:9080/"))
+				.route("book-app-actuator-refresh-route",
+						r -> r.path("/actuator/refresh")
+							.and().header("X-Hub-Signature-256")
+							.and().predicate(exchange -> {
+								String hashedBody = exchange.getRequest().getHeaders().getFirst("X-Hub-Signature-256");
+								System.out.println("Hashed Body: " + hashedBody);
+								return true;
+							})
+						.filters(
+							f -> f.modifyRequestBody(String.class, String.class, (exchange, s) -> Mono.empty()))
+						.uri("http://localhost:9080"))
 				.build();
 	}
 
