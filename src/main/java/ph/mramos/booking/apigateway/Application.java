ca1781bd5c1683987e6e11277cd25530662568d8
Max <Max@MAX-LENOVO>
2022-07-06 16:04:11 +0800

    Rename application to match spring cloud diagram.

--- src/main/java/ph/mramos/booking/apigateway/Application.java
+++ src/main/java/ph/mramos/booking/apigateway/Application.java
@@ -1,4 +1,4 @@
-package ph.mramos.booking.gatewayserver;
+package ph.mramos.booking.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
